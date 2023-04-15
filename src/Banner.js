19b9b5558746f4f2be22a781757fb4748d7d92b3
Leslie Lopez <leslie2515lopez@gmail.com>
2023-04-14 23:35:07 -0600

    Updated banner file

--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./Banner.css";
-import axios from "axios";
+import axios from "./axios";
 import requests from "./Requests";
 
 function Banner() {
@@ -18,7 +18,7 @@ function Banner() {
     }
 
     fetchData();
-  });
+  }, []);
 
   function truncate(string, n) {
     return string?.length > n ? string.substr(0, n - 1) + "..." : string;
